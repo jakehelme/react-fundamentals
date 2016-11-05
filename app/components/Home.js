5bc7830dd583db508617ee03538dd133c7e9e226
Jake Helme <Jake Helme>
2016-11-06 01:09:18 +0200

    Added prompt containers and started using routing and prop types.

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,10 +1,16 @@
 var React = require('react');
+var transparentBg = require('../styles').transparentBg;
+var Link = require('react-router').Link;
 
 var Home = React.createClass({
   render: function () {
     return (
-      <div>
-        Hello from home
+      <div className="jumbotron col-sm-12 text-center" style={transparentBg} >
+        <h1>GitHub Battle</h1>
+        <p className="lead">Some fancy stuff</p>
+        <Link to='/playerOne'>
+          <button className='btn btn-lg btn-success' type='button'>Get Started</button>
+        </Link>
       </div>
     )
   }
