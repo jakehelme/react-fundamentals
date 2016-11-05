ac588d0b1f1291bd782134f064e0f3751637b5d7
Jake Helme <jakehelme@users.noreply.github.com>
2016-11-05 22:39:36 +0200

    Video3 (#2)
    * Added first basic components with dummy data
    
    * Trying to get line endings right

--- app/index.js
+++ app/index.js
@@ -1,15 +1,52 @@
+var USER_DATA = {
+    name: 'Jake Helme',
+    username: 'jakehelme',
+    image: "https://www.gravatar.com/avatar/471d7e574b79e8d4aa465fd7955a083d?s=100"
+};
+
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var HelloWorld = React.createClass({
-    render: function(){
-        return(
-            <div> Hello Jake </div>
+var ProfilePic = React.createClass({
+    render: function () {
+        return <img src={this.props.imageUrl} style={{ height: 100, width: 100 }} />
+    }
+});
+
+var ProfileLink = React.createClass({
+    render: function () {
+        return (
+            <div>
+                <a href={'https://github.com/' + this.props.username}>
+                    {this.props.username}
+                </a>
+            </div>
         )
     }
 });
 
+var ProfileName = React.createClass({
+    render: function(){
+        return (
+            <div>{this.props.username}</div>
+        )
+    }
+});
+
+var Avatar = React.createClass({
+    render: function(){
+        return (
+            <div>
+                <ProfilePic imageUrl={this.props.user.image} />
+                <ProfileName name={this.props.user.name} />
+                <ProfileLink username={this.props.user.username} />
+            </div>
+        )
+    }
+});
+
+
 ReactDOM.render(
-    <HelloWorld />,
+    <Avatar user={USER_DATA} />,
     document.getElementById('app')
 );
