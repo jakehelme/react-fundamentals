53d2890329d3f32a4f69c019001e00c2e52194e2
Jake Helme <jakehelme@users.noreply.github.com>
2016-11-06 16:37:23 +0200

    Finished off results page and some refactoring. (#9)

--- app/components/ConfirmBattle.js
+++ app/components/ConfirmBattle.js
@@ -4,38 +4,34 @@ var PropTypes = React.PropTypes;
 var styles = require('../styles');
 var UserDetails = require('../components/UserDetails');
 var UserDetailsWrapper = require('./UserDetailsWrapper');
-
-function puke (object) {
-  return <pre>{JSON.stringify(object, null, ' ')}</pre>
-}
+var MainContainer = require('./MainContainer');
 
 function ConfirmBattle(props) {
   return props.isLoading === true
     ? <p> LOADING !</p>
-    : <div className="jumbotron col-sm-12 text-center" style={styles.transparentBg}>
-        <h1>Confirm Players</h1>
-        <div className="col-sm-8 col-sm-offset-2">
-          <UserDetailsWrapper header='Player One'>
-            <UserDetails info={props.playersInfo[0]}/>
-          </UserDetailsWrapper>
-          <UserDetailsWrapper header='Player Two'>
-            <UserDetails info={props.playersInfo[1]}/>
-          </UserDetailsWrapper>
-        </div>
-        <div className="col-sm-8 col-sm-offset-2">
-          <div className="col-sm-12" style={styles.space}>
-            <button className="btn btn-lg btn-success" type="button" onClick={props.onInitiateBattle}>
-              Initiate Battle!
+    : <MainContainer>
+      <h1>Confirm Players</h1>
+      <div className="col-sm-8 col-sm-offset-2">
+        <UserDetailsWrapper header='Player One'>
+          <UserDetails info={props.playersInfo[0]} />
+        </UserDetailsWrapper>
+        <UserDetailsWrapper header='Player Two'>
+          <UserDetails info={props.playersInfo[1]} />
+        </UserDetailsWrapper>
+      </div>
+      <div className="col-sm-8 col-sm-offset-2">
+        <div className="col-sm-12" style={styles.space}>
+          <button className="btn btn-lg btn-success" type="button" onClick={props.onInitiateBattle}>
+            Initiate Battle!
             </button>
-          </div>
-          <div className="col-sm-12" style={styles.space}>
-            <Link to='/playerOne'>
-              <button type='button' className='btn btn-lg btn-danger'>Reselect Players</button>
-            </Link>
-          </div>
         </div>
-      </div> 
-    
+        <div className="col-sm-12" style={styles.space}>
+          <Link to='/playerOne'>
+            <button type='button' className='btn btn-lg btn-danger'>Reselect Players</button>
+          </Link>
+        </div>
+      </div>
+    </MainContainer>
 }
 
 ConfirmBattle.propTypes = {
--- app/components/Home.js
+++ app/components/Home.js
@@ -1,17 +1,17 @@
 var React = require('react');
-var transparentBg = require('../styles').transparentBg;
 var Link = require('react-router').Link;
+var MainContainer = require('./MainContainer');
 
 var Home = React.createClass({
   render: function () {
     return (
-      <div className="jumbotron col-sm-12 text-center" style={transparentBg} >
+      <MainContainer>
         <h1>GitHub Battle</h1>
         <p className="lead">Some fancy stuff</p>
         <Link to='/playerOne'>
           <button className='btn btn-lg btn-success' type='button'>Get Started</button>
         </Link>
-      </div>
+      </MainContainer>
     )
   }
 });
