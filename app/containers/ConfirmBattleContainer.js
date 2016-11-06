3e8a0d598da525c3d19fd8388fcb3f15d00514a4
Jake Helme <jake.helme@gmail.com>
2016-11-06 12:41:11 +0200

    Added axios for GitHub API calls

--- app/containers/ConfirmBattleContainer.js
+++ app/containers/ConfirmBattleContainer.js
@@ -1,33 +1,28 @@
 var React = require('react');
 var ConfirmBattle = require('../components/ConfirmBattle');
+var gitHubHelpers = require('../utils/gitHubHelpers');
 
 var ConfirmBattleContainer = React.createClass({
   contextTypes: {
     router: React.PropTypes.object.isRequired
   },
   getInitialState: function(){
-    console.log('getInitialState');
     return {
       isLoading: true,
       playerInfo: []
     }
   },
-  componentWillMount: function(){
-    console.log('componentWillMount');
-  },
   componentDidMount: function(){
     var query = this.props.location.query;
-    console.log('componentDidMount');
-    // TODO: Fetch info from GitHub
-  },
-  componentWillReceiveProps: function(){
-    console.log('componentWillReceiveProps');
-  },
-  componentWillUnmount: function(){
-    console.log('componentWillUnmount');
+    gitHubHelpers.getPlayersInfo([query.playerOne, query.playerTwo])
+      .then(function(players){
+        this.setState({
+          isLoading: false,
+          playersInfo: [players[0], players[1]]
+        })
+      }.bind(this))
   },
   render: function(){
-    console.log('render');
     return (
       <ConfirmBattle 
         isLoading={this.state.isLoading}
